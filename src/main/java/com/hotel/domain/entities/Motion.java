c217ca071e50ac0415e9d90337ac53c2e4028e23
Pulkit Gupta-GGN <Pulkit_Gupta-GGN@external.mckinsey.com>
2020-11-01 23:40:43 +0530

    Add Motion.java

---
+++ src/main/java/com/hotel/domain/entities/Motion.java
@@ -0,0 +1,23 @@
+package com.hotel.domain.entities;
+
+import com.hotel.domain.constants.MotionType;
+
+public class Motion {
+    private final Floor floor;
+    private final Corridor corridor;
+    private final MotionType type;
+
+    public Motion(Floor floor, Corridor corridor, MotionType type) {
+        this.floor = floor;
+        this.corridor = corridor;
+        this.type = type;
+    }
+
+    public Corridor getCorridor() {
+        return corridor;
+    }
+
+    public MotionType getType() {
+        return type;
+    }
+}
