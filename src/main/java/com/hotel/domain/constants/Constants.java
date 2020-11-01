5677a117f597dfcbb5de85aa626d489c946f1051
Pulkit Gupta-GGN <Pulkit_Gupta-GGN@external.mckinsey.com>
2020-11-01 23:34:26 +0530

    Add Constants.java

---
+++ src/main/java/com/hotel/domain/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.hotel.domain.constants;
+
+public class Constants {
+    public static final Integer AC_POWER_CONSUMPTION = 10;
+    public static final Integer LIGHT_POWER_CONSUMPTION = 5;
+    public static final Integer MAIN_CORRIDOR_RATE = 15;
+    public static final Integer SUB_CORRIDOR_RATE = 10;
+
+    private Constants() {
+        /* Private contr so as to stop any dev from making
+        object of this class by default public contr  */
+    }
+}
