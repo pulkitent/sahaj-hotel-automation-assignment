22975666b556f0f448fa2a5d008d25e96fd0686e
Pulkit Gupta-GGN <Pulkit_Gupta-GGN@external.mckinsey.com>
2020-11-01 23:37:59 +0530

    Add Consumption.java

---
+++ src/main/java/com/hotel/domain/entities/Consumption.java
@@ -0,0 +1,13 @@
+package com.hotel.domain.entities;
+
+public class Consumption {
+    private final Integer value;
+
+    public Consumption(Integer value) {
+        this.value = value;
+    }
+
+    public Integer value() {
+        return value;
+    }
+}
